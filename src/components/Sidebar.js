fdec04cabd9be117687281b21a34d53ecbe6c214
jack <actionjacks22@gmail.com>
2020-10-28 22:34:02 +0100

    add sidebar and sidebarOptions

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "../styles/Sidebar.css";
+import SidebarOption from "./SidebarOption";
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <img className="sidebar__logo" src="./logo_body.png" alt="spotify" />
+      <SidebarOption Icon={SearchIcon} title="home" />
+      <SidebarOption Icon={HomeIcon} title="search" />
+      <SidebarOption Icon={LibraryMusicIcon} title="U library" />
+      <br />
+      <strong className="sidebar__title">Play-List</strong>
+      <hr />
+      <SidebarOption title="disco" />
+      <SidebarOption title="disco" />
+      <SidebarOption title="disco" />
+      <SidebarOption title="disco" />
     </div>
   );
 }
